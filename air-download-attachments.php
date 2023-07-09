58eccf8b1e4ae2d4651e7a5661bdab8e5332ed79
Dan Zakirov <alexodiy@gmail.com>
2023-07-09 12:20:36 +0300

    Description for WordPress repository

--- air-download-attachments.php
+++ air-download-attachments.php
@@ -1,11 +1,35 @@
 <?php
 /**
- * Plugin Name: Air Download Attachments
+ * Plugin Name: AIR Download Attachments
+ * Plugin URI: https://wordpress.org/plugins/air-download-attachments/
  * Description: Adds a "Download All Attachments" button to posts, allowing users to download all attached images as a zip archive.
  * Version: 1.0.0
  * Author: Dan Zakirov
+ * Author URI: https://profiles.wordpress.org/alexodiy/
  * Text Domain: air-download-attachments
  * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * WP tested up to: 6.2.2
+ *
+ *     Copyright Dan Zakirov
+ *
+ *     This file is part of AIR Download Attachments,
+ *     a plugin for WordPress.
+ *
+ *     AIR Download Attachments is free software:
+ *     You can redistribute it and/or modify it under the terms of the
+ *     GNU General Public License as published by the Free Software
+ *     Foundation, either version 3 of the License, or (at your option)
+ *     any later version.
+ *
+ *     AIR Download Attachments is distributed in the hope that
+ *     it will be useful, but WITHOUT ANY WARRANTY; without even the
+ *     implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with WordPress. If not, see <http://www.gnu.org/licenses/>.
  *
  * @package Air_Download_Attachments
  */
